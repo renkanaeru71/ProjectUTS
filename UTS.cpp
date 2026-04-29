cc5c8fecce4ae5633365e3b99d2a04126278770e
renkanaeru71 <rizkypra1156@gmail.com>
2026-04-29 20:08:11 +0700

    claude

--- UTS.cpp
+++ UTS.cpp
@@ -1,100 +1,140 @@
 //kelompok 11: 123250061, 123250065, 123250066
+
 #include <iostream>
-#include <iomanip>
+#include <string>
 using namespace std;
 
-void searching (){
-	char ulang;
-	int pilih;
-	
-	do{
-		system("cls");
-		cout << "MENU SEARCHING\n";
-		cout << "=========================\n";
-		cout << "1. Sequential\n";
-		cout << "2. Binary\n";
-		cout << "3. EXIT\n";
-		cout << "=========================\n";
-		cout << "Pilih: ";
-		cin >> pilih;  
-		cout << endl;
-		
-		if (pilih == 1)
-		{
-			seqsearching();
-			}
-		else if (pilih == 2)
-		{
-			BubbleS();
-			binary();
-			dataawal();
-			}
-		else if (pilih == 3)
-		{
-			ulang = 't';
-			}
-		else 
-		{
-			cout << "Pilihan di menu searching tidak ada...\n";
-			}
-			
-		if (pilih != 3)
-		{
-			cout << "Ulangi (y/t) : ";
-			cin >> ulang;
-			}
-		} while (ulang == 'y' || ulang == 'Y');
-	}
-	
-	
-	
-	}
+typedef struct {
+    long NoPendaftaran;
+    string Nama;
+    string Tanggal;
+} pendaftaran;
 
-int main (){
-	int pilih;
-	cout << "PROGRAM TUGAS\n";
-	
-	do{
-		system("cls");
-		cout << "MENU\n";
-		cout << "PROGRAM TUGAS\n";
-		cout << "=========================\n";
-		cout << "1. INPUT DATA\n";
-		cout << "2. TAMPIL DATA\n";
-		cout << "3. SEARCHING\n";
-		cout << "4. SORTING\n";
-		cout << "5. EXIT\n";
-		cout << "=========================\n";
-		cout << "Pilih: ";
-		cin >> pilih;  
-		
-		switch(pilih){
-			case 1 : 
-				cout << "INPUT DATA\n";
-			//	input();
-				break;
-			case 2: 
-				cout << "TAMPIL DATA\n";
-			//	tampil();
-				break;
-			case 3:
-				cout << "SEARCHING\n";
-			//	searching();
-				break;
-			case 4: 
-				cout << "SORTING\n";
-			//	checkreq();
-				break;
-			case 5: 
-				cout << "Exiting program...\n";
-				break;
-			default:
-				cout << "PILIHAN TIDAK VALID\n";
-			
-			}
-		
+pendaftaran data[100];
+int jumlah = 0;
 
-		} while (pilih !=5);
-		
-		return 0;
-	}
+void tambahData() {
+    cout << "\n=== TAMBAH DATA ===" << endl;
+    cout << "No Pendaftaran       : ";
+    cin >> data[jumlah].NoPendaftaran;
+    cin.ignore();
+    cout << "Nama                 : ";
+    getline(cin, data[jumlah].Nama);
+    cout << "Tanggal (DD/MM/YYYY) : ";
+    getline(cin, data[jumlah].Tanggal);
+    jumlah++;
+    cout << "Data berhasil ditambahkan!" << endl;
+}
+
+void tampilData() {
+    cout << "\n=== DAFTAR DATA PENDAFTARAN ===" << endl;
+    if (jumlah == 0) {
+        cout << "Belum ada data." << endl;
+        return;
+    }
+    cout << "--------------------------------------------------------------" << endl;
+    cout << "No\tNo PendaftaranNama\t\t\tTanggal" << endl;
+    cout << "--------------------------------------------------------------" << endl;
+    for (int i = 0; i < jumlah; i++) {
+        cout << i + 1 << "\t"
+             << data[i].NoPendaftaran << "\t\t"
+             << data[i].Nama << "\t\t\t"
+             << data[i].Tanggal << endl;
+    }
+    cout << "--------------------------------------------------------------" << endl;
+}
+
+void cariData() {
+    long cari;
+    bool ketemu = false;
+    cout << "\n=== CARI DATA ===" << endl;
+    cout << "Masukkan No Pendaftaran yang dicari: ";
+    cin >> cari;
+    for (int i = 0; i < jumlah; i++) {
+        if (data[i].NoPendaftaran == cari) {
+            cout << "\nData ditemukan:" << endl;
+            cout << "No Pendaftaran : " << data[i].NoPendaftaran << endl;
+            cout << "Nama           : " << data[i].Nama << endl;
+            cout << "Tanggal        : " << data[i].Tanggal << endl;
+            ketemu = true;
+            break;
+        }
+    }
+    if (!ketemu) {
+        cout << "Data dengan No Pendaftaran " << cari << " tidak ditemukan." << endl;
+    }
+}
+
+void hapusData() {
+    long hapus;
+    int ketemu = -1;
+    cout << "\n=== HAPUS DATA ===" << endl;
+    cout << "Masukkan No Pendaftaran yang akan dihapus: ";
+    cin >> hapus;
+    for (int i = 0; i < jumlah; i++) {
+        if (data[i].NoPendaftaran == hapus) {
+            ketemu = i;
+            break;
+        }
+    }
+    if (ketemu == -1) {
+        cout << "Data tidak ditemukan." << endl;
+        return;
+    }
+    for (int i = ketemu; i < jumlah - 1; i++) {
+        data[i] = data[i + 1];
+    }
+    jumlah--;
+    cout << "Data berhasil dihapus!" << endl;
+}
+
+void editData() {
+    long edit;
+    int ketemu = -1;
+    cout << "\n=== EDIT DATA ===" << endl;
+    cout << "Masukkan No Pendaftaran yang akan diedit: ";
+    cin >> edit;
+    for (int i = 0; i < jumlah; i++) {
+        if (data[i].NoPendaftaran == edit) {
+            ketemu = i;
+            break;
+        }
+    }
+    if (ketemu == -1) {
+        cout << "Data tidak ditemukan." << endl;
+        return;
+    }
+    cin.ignore();
+    cout << "Nama baru (sebelumnya: " << data[ketemu].Nama << "): ";
+    getline(cin, data[ketemu].Nama);
+    cout << "Tanggal baru (sebelumnya: " << data[ketemu].Tanggal << "): ";
+    getline(cin, data[ketemu].Tanggal);
+    cout << "Data berhasil diperbarui!" << endl;
+}
+
+int main() {
+    int pilihan;
+    do {
+        cout << "\n=== MENU UTAMA ===" << endl;
+        cout << "1. Tambah Data" << endl;
+        cout << "2. Tampil Data" << endl;
+        cout << "3. Cari Data" << endl;
+        cout << "4. Edit Data" << endl;
+        cout << "5. Hapus Data" << endl;
+        cout << "0. Keluar" << endl;
+        cout << "Pilihan: ";
+        cin >> pilihan;
+
+        switch (pilihan) {
+            case 1: tambahData(); break;
+            case 2: tampilData(); break;
+            case 3: cariData();   break;
+            case 4: editData();   break;
+            case 5: hapusData();  break;
+            case 0: cout << "Terima kasih!" << endl; break;
+            default: cout << "Pilihan tidak valid." << endl;
+        }
+    } while (pilihan != 0);
+
+    return 0;
+}
